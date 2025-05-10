b74b3e82213ee70910ea427f252838908774808b
Attos <attosius@gmail.com>
2025-05-10 16:57:25 +0300

    Add person view, add world

---
+++ Pseudo3dEngine/Person.cs
@@ -0,0 +1,84 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace Pseudo3dEngine;
+
+public class Person : Drawable
+{
+    public Vector2f Position { get; set; } = new Vector2f(100, 0);
+    public double Fov { get; set; } = 3.14 / 3;
+    public float Direction { get; set; } = 0;
+    public float Speed = 6f;
+    public float SpeedTurn = 0.1f;
+
+    public float SpeedStrafe = 5f;
+    public float DistanceView = 200f;
+
+    public float DirectionDegree => Direction * 180 / (float)Math.PI;
+
+    public void Draw(RenderTarget target, RenderStates states)
+    {
+        var radius = DrawPerson(target);
+
+        var center = new Vector2f(Position.X + radius, Position.Y + radius);
+
+        DrawDirection(target, states, center);
+
+        var leftViewAngle = Direction - Fov / 2;
+        var leftPoint = GetPointAtAngleAndDistance(center, leftViewAngle, DistanceView);
+        var leftLineView = new Vertex[]
+        {
+            new(center),
+            new(leftPoint),
+        };
+        target.Draw(leftLineView, PrimitiveType.LineStrip, states);
+        var rightViewAngle = Direction + Fov / 2;
+        var rightPoint = GetPointAtAngleAndDistance(center, rightViewAngle, DistanceView);
+        var rightLineView = new Vertex[]
+        {
+            new(center),
+            new(rightPoint),
+        };
+        target.Draw(rightLineView, PrimitiveType.LineStrip, states);
+
+        var delta = Fov / 9;
+        var currentAngle = leftViewAngle;
+        var circleRadius = new Vertex[10];
+        for (int i = 0; i < 10; i++)
+        {
+            var point = GetPointAtAngleAndDistance(center, currentAngle, DistanceView);
+            circleRadius[i] = new Vertex(point);
+            currentAngle += delta;
+        }
+        target.Draw(circleRadius, PrimitiveType.LineStrip, states);
+    }
+
+    private static Vector2f GetPointAtAngleAndDistance(Vector2f center, double angle, float distance)
+    {
+        var x = center.X + (float)Math.Sin(angle) * distance;
+        var y = center.Y + (float)Math.Cos(angle) * distance;
+        return new Vector2f(x, y);
+    }
+
+    private void DrawDirection(RenderTarget target, RenderStates states, Vector2f center)
+    {
+        var xDir = center.X + (float)Math.Sin(Direction) * 50f;
+        var yDir = center.Y + (float)Math.Cos(Direction) * 50f;
+        var lineArr = new Vertex[]
+        {
+            new(center),
+            new(new Vector2f(xDir, yDir)),
+        };
+        target.Draw(lineArr, PrimitiveType.LineStrip, states);
+    }
+
+    private float DrawPerson(RenderTarget target)
+    {
+        var radius = 10f;
+        var person = new CircleShape(radius);
+        person.FillColor = Color.Red;
+        person.Position = Position;
+        target.Draw(person);
+        return radius;
+    }
+}
--- Pseudo3dEngine/Program.cs
+++ Pseudo3dEngine/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
@@ -21,117 +22,55 @@ namespace Pseudo3dEngine
 
                 var font = new Font("d:\\Projects\\Experimentals\\Pseudo3dEngine\\cour.ttf");
 
-                var circleShape = new CircleShape(50);
-                circleShape.FillColor = Color.Green;
+                //var circleShape = new CircleShape(50);
+                //circleShape.FillColor = Color.Green;
+                
+                //var rectangle = new RectangleShape(new Vector2f(120f, 50f));
+                //rectangle.FillColor = new Color(255, 175, 174);
+                //rectangle.Size = new Vector2f(10, 10);
+                //rectangle.Position = new Vector2f(0, 0);
+                //rectangle.OutlineThickness = 2;
+                //rectangle.OutlineColor = new Color(255, 255, 255);
+                
+                //var line = new RectangleShape (new Vector2f(10, 133));
+                //line.Rotation = 45;
 
 
-                var rectangle = new RectangleShape(new Vector2f(120f, 50f));
-                rectangle.FillColor = new Color(255, 175, 174);
-                rectangle.Size = new Vector2f(10, 10);
-                rectangle.Position = new Vector2f(0, 0);
-                rectangle.OutlineThickness = 2;
-                rectangle.OutlineColor = new Color(255, 255, 255);
-
-
-
-                var line = new RectangleShape (new Vector2f(10, 133));
-                line.Rotation = 45;
-
-
-                var lineArr = new Vertex[]
-                {
-                    new Vertex(new Vector2f(10, 50)),
-                    new Vertex(new Vector2f(150, 150)),
-                    new Vertex(new Vector2f(50, 10))
-                };
+                //var lineArr = new Vertex[]
+                //{
+                //    new Vertex(new Vector2f(10, 50)),
+                //    new Vertex(new Vector2f(150, 150)),
+                //    new Vertex(new Vector2f(50, 10))
+                //};
 
                 var person = new Person();
-
-                var cam = new CameraMan();
+                
                 var sw = new Stopwatch();
+                var world = new World();
                 // Start the game loop
                 while (window.IsOpen)
                 {
-                    var elapsed = sw.ElapsedMilliseconds / (double)1000; ;
+                    var elapsed = sw.ElapsedMilliseconds / (double)1000;
                     sw.Restart();
                     Thread.Sleep(10);
 
                     window.DispatchEvents();
                     window.Closed += (sender, _) => ((RenderWindow)sender!).Close();
+                    
+                    InputEvents(person);
 
-
-                    if (Keyboard.IsKeyPressed(Keyboard.Key.W) )
-                    {
-                        var personPosition = person.Position;
-                        personPosition.X += (float)Math.Sin(person.Direction) * person.Speed;
-                        personPosition.Y += (float)Math.Cos(person.Direction) * person.Speed;
-                        person.Position = personPosition;
-                    }
-                    if (Keyboard.IsKeyPressed(Keyboard.Key.S))
-                    {
-                        var personPosition = person.Position;
-                        personPosition.X -= (float)Math.Sin(person.Direction) * person.Speed;
-                        personPosition.Y -= (float)Math.Cos(person.Direction) * person.Speed;
-                        person.Position = personPosition;
-                    }
-
-                    if ( Keyboard.IsKeyPressed(Keyboard.Key.Up))
-                    {
-                        var personPosition = person.Position;
-                        personPosition.Y -= person.SpeedStrafe;
-                        person.Position = personPosition;
-                    }
-                    if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
-                    {
-                        var personPosition = person.Position;
-                        personPosition.Y += person.SpeedStrafe;
-                        person.Position = personPosition;
-                    }
-
-
-                    if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
-                    {
-                        var personPosition = person.Position;
-                        personPosition.X -= person.SpeedStrafe;
-                        person.Position = personPosition;
-                    }
-                    if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
-                    {
-                        var personPosition = person.Position;
-                        personPosition.X += person.SpeedStrafe;
-                        person.Position = personPosition;
-                    }
-
-                    if (Keyboard.IsKeyPressed(Keyboard.Key.A))
-                    {
-                        person.Direction += person.SpeedTurn;
-                    }
-                    if (Keyboard.IsKeyPressed(Keyboard.Key.D))
-                    {
-                        person.Direction -= person.SpeedTurn;
-                    }
-                    // Clear screen
                     window.Clear();
-
-                    // Draw the sprite
+                    
                     //window.Draw(sprite);
-
-                    // Draw the string
-                    window.Draw(circleShape);
-                    window.Draw(rectangle);
-                    window.Draw(person);
+                    
+                    //window.Draw(circleShape);
+                    //window.Draw(rectangle);
                     //window.Draw(line);
-                    window.Draw(cam);
+                    //window.Draw(cam);
                     //window.Draw(lineArr, PrimitiveType.LineStrip);
-                    // Update the window
-                    var fps = 1d / elapsed;
-                    //Console.WriteLine(fps);
-                    var text = new Text($"Position: X ({person.Position.X:000.00}) Y ({person.Position.Y:000.00})," +
-                                        $" angle: {person.DirectionDegree:000.00}, FPS: {fps:00.00}", font, 12);
-                    text.Position = new Vector2f(0, 0);
-                    text.FillColor = Color.White;
-                    //text.OutlineThickness = 0.1f;
-                    window.Draw(text);
+                    window.Draw(person);
+                    ShowStatistic(elapsed, person, font, window);
+                    window.Draw(world);
 
                     window.Display();
                 }
@@ -142,38 +81,119 @@ namespace Pseudo3dEngine
                 Console.WriteLine(e);
             }
         }
-        
+
+        private static void InputEvents(Person person)
+        {
+            if (Keyboard.IsKeyPressed(Keyboard.Key.W))
+            {
+                var personPosition = person.Position;
+                personPosition.X += (float)Math.Sin(person.Direction) * person.Speed;
+                personPosition.Y += (float)Math.Cos(person.Direction) * person.Speed;
+                person.Position = personPosition;
+            }
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.S))
+            {
+                var personPosition = person.Position;
+                personPosition.X -= (float)Math.Sin(person.Direction) * person.Speed;
+                personPosition.Y -= (float)Math.Cos(person.Direction) * person.Speed;
+                person.Position = personPosition;
+            }
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
+            {
+                var personPosition = person.Position;
+                personPosition.Y -= person.SpeedStrafe;
+                person.Position = personPosition;
+            }
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
+            {
+                var personPosition = person.Position;
+                personPosition.Y += person.SpeedStrafe;
+                person.Position = personPosition;
+            }
+
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
+            {
+                var personPosition = person.Position;
+                personPosition.X -= person.SpeedStrafe;
+                person.Position = personPosition;
+            }
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
+            {
+                var personPosition = person.Position;
+                personPosition.X += person.SpeedStrafe;
+                person.Position = personPosition;
+            }
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.A))
+            {
+                person.Direction += person.SpeedTurn;
+            }
+
+            if (Keyboard.IsKeyPressed(Keyboard.Key.D))
+            {
+                person.Direction -= person.SpeedTurn;
+            }
+        }
+
+        private static void ShowStatistic(double elapsed, Person person, Font font, RenderWindow window)
+        {
+            var fps = 1d / elapsed;
+            //Console.WriteLine(fps);
+            var text = new Text($"Position: X ({person.Position.X:000.00}) Y ({person.Position.Y:000.00})," +
+                                $" angle: {person.DirectionDegree:000.00}, FPS: {fps:00.00}", font, 12);
+            text.Position = new Vector2f(0, 0);
+            text.FillColor = Color.White;
+            //text.OutlineThickness = 0.1f;
+            window.Draw(text);
+        }
     }
-    public class Person : Drawable
+
+    public class Object2d : Drawable
     {
-        public Vector2f Position { get; set; }
-        public double Fov { get; set; } = 3.14 / 3;
-        public float Direction { get; set; } = 0;
-        public float Speed = 6f;
-        public float SpeedTurn = 0.1f;
-
-        public float SpeedStrafe = 5f;
-
-        public float DirectionDegree => Direction * 180 / (float)Math.PI;
+        public List<Vector2f> Points = new ();
 
         public void Draw(RenderTarget target, RenderStates states)
         {
-            var radius = 10f;
-            var person = new CircleShape(radius);
-            person.FillColor = Color.Red;
-            person.Position = Position;
-            target.Draw(person);
-
-            var center = new Vector2f(Position.X + radius, Position.Y + radius);
-
-            var xDir = center.X + (float)Math.Sin(Direction) * 50f;
-            var yDir = center.Y + (float)Math.Cos(Direction) * 50f;
-            var lineArr = new Vertex[]
+            var convexShape = new ConvexShape();
+            convexShape.SetPointCount((uint)Points.Count);
+            for (var i = 0; i < Points.Count; i++)
             {
-                new(center),
-                new(new Vector2f(xDir, yDir)),
-            };
-            target.Draw(lineArr, PrimitiveType.LineStrip, states);
+                var vector2F = Points[i];
+                convexShape.SetPoint((uint)i, vector2F);
+            }
+            convexShape.FillColor = new Color(255, 175, 174);
+            convexShape.OutlineThickness = 2;
+            convexShape.OutlineColor = new Color(255, 255, 255);
+            target.Draw(convexShape);
+        }
+    }
+
+    public class World : Drawable
+    {
+        public List<Object2d> Objects = new List<Object2d>();
+
+        public World()
+        {
+            var square = new Object2d();
+            square.Points.Add(new Vector2f(50, 150));
+            square.Points.Add(new Vector2f(100, 150));
+            square.Points.Add(new Vector2f(100, 200));
+            square.Points.Add(new Vector2f(50, 200));
+            Objects.Add(square);
+        }
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            foreach (var object2d in Objects)
+            {
+
+                target.Draw(object2d);
+            }
         }
     }
 
